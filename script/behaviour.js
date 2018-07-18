e1c345750488efadfa99f7193cfbd2aef3e4a090
Unknown <m.kramek@outlook.com>
2018-07-18 10:55:57 +0200

    File configuration change

---
+++ script/behaviour.js
@@ -0,0 +1,29 @@
+$(function() {
+	var $image = $("#header_image");
+	var $menu_container = $("#menu_container");
+	var $page_container = $("#page_container");
+
+	$(window).scroll(function() {
+		var scroll = $(window).scrollTop();
+		if (scroll >= 225) {
+			console.log("Scroll exceeded.");
+			$menu_container.removeClass("mc_not_scrolled").addClass("mc_scrolled");
+			$image.removeClass("hi_not_scrolled").addClass("hi_scrolled");
+			$page_container.css("margin-top","150px");
+		} else {
+			$menu_container.removeClass("mc_scrolled").addClass("mc_not_scrolled");
+			$image.removeClass("hi_scrolled").addClass("hi_not_scrolled");
+			$page_container.css("margin-top","0");
+		}
+	});
+});
+
+function resizeIframe(iframe) {
+	iframe.height = iframe.contentWindow.document.body.scrollHeight + "px";
+}
+
+function prepare() {
+	var outer = document.getElementById('outer');
+	var inner = document.getElementById('inner');
+	inner.style.right = inner.clientWidth - inner.offsetWidth + "px";
+}
