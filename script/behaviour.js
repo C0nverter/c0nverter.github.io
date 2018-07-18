c6ced292cf49d026e07cbed026460ac9e927f022
Unknown <m.kramek@outlook.com>
2018-07-18 22:11:01 +0200

    Added contact info and "About" section

--- script/behaviour.js
+++ script/behaviour.js
@@ -2,11 +2,19 @@ $(function() {
 	var $image = $("#header_image");
 	var $menu_container = $("#menu_container");
 	var $page_container = $("#page_container");
+	var paragraphs = $('p.content');
+	var headers = $('h1.header');
+
+	paragraphs.fadeIn(1000);
+	headers.fadeIn(1000);
+
+	$(document).delegate('div.card', 'click', function() {
+		window.location = $(this).find("a.data").attr('href');
+	});
 
 	$(window).scroll(function() {
 		var scroll = $(window).scrollTop();
 		if (scroll >= 225) {
-			console.log("Scroll exceeded.");
 			$menu_container.removeClass("mc_not_scrolled").addClass("mc_scrolled");
 			$image.removeClass("hi_not_scrolled").addClass("hi_scrolled");
 			$page_container.css("margin-top","150px");
